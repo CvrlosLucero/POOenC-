fd30259661e187b561718a8dc6cd6c069386b342
CvrlosLucero <carloslucerotobar001@gmail.com>
2024-03-06 14:29:41 -0500

    Create Inicio.cpp

---
+++ Principal/Inicio.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+class Inicio{
+    Persona carlitos;
+};
+
+int main(){
+
+    carlitos.pedirDatos();
+    carlitos.calcularImc();
+    carlitos.mostrarPersona();
+}
