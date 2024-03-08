8b758620c37e1cedc22e14137bb7e2c3e742c874
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-03-08 00:18:47 -0500

    Added execution to Inicio.cpp

--- Principal/Inicio.cpp
+++ Principal/Inicio.cpp
@@ -1,25 +1,52 @@
 #include <iostream>
 #include <math.h>
-#include "Persona.cpp"
+#include "../Salud/Persona.cpp"
 
 using namespace std;
 
 class Inicio{
-    Persona user;
+
+    Persona carlitos;
 
 };
 
 int main(){
 
     Persona carlitos;
+    int eleccion;
+    bool bucle = true;
+
+        while(bucle=true){
+            cout<<"Sistema de peso ideal.\nElige que necesitas hacer: \n(1) Para pedir datos de la persona.\n(2) Para mostrar los datos pedidos de la persona.\n(3) Calcular si el peso es ideal.\n(4) Mostrar si la persona es mayor de edad."<<endl;
+                cin>>eleccion;
+
+            switch(eleccion){
+                case 1:{
+                    carlitos.pedirDatos();
+                }
+                case 2:{
+                    carlitos.mostrarPersona();
+                }
+                case 3:{
+                    carlitos.calcularImc();
+        
+                    if(carlitos.calcularImc()=="PESOBAJO"){
+                        cout<<"El peso está por debajo de lo ideal."<<endl;
+                    }else if(carlitos.calcularImc()=="PESOIDEAL"){
+                        cout<<"El peso es ideal."<<endl;
+                    }else if(carlitos.calcularImc()=="SOBREPESO"){
+                        cout<<"Tiene sobrepeso."<<endl;
+                    }
+                }
+                case 4:{
+                    carlitos.mayorEdad();
+                }
+                default:{
+                    cout<<"Elección incorrecta."<<endl;
+                    return main();
+                }
+             }
+         }
     
-    if(carlitos.calcularImc()=="PESOBAJO"){
-        cout<<"El peso está por debajo de lo ideal."<<endl;
-    }else if(carlitos.calcularImc()=="PESOIDEAL"){
-        cout<<"El peso es ideal."<<endl;
-    }else if(carlitos.calcularImc()=="SOBREPESO"){
-        cout<<"Tiene sobrepeso."<<endl;
-    }
-   
     return 0;
 }
