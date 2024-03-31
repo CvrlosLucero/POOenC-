104e263255af7678383d86e8284ccbb3bdf058dd
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-03-31 16:34:19 -0500

    Created class Song and Node
    By creating class song, now every song added in a list will include a name, an artist, and a duration each one. Changed type of list from string to Song and changed every atribute and variable from string to song. And now whenever you want to add a song via conditionals, you can stop adding them. In class node, atributes were made private to use setters and getters for higher convenience.

--- Salud/playlist.cpp
+++ Salud/playlist.cpp
@@ -5,81 +5,131 @@
 
 using namespace std;
 
-struct Node {
-    string data;
-    Node* next;
+class Song {
+    public:
+        string nombre;
+        string artista;
+        int duracion;
+    public:
+        Song(const string& nombre = "", const string& artista = "", int duracion = 0)
+        : nombre(nombre), artista(artista), duracion(duracion) {}
+        
+        friend istream& operator>>(istream& is, Song& song) {
+            cout<<"Nombre: ";
+                is>>song.nombre;
+                if(song.nombre=="X"){
+                    return is;
+                }
+            cout<<"Artista: ";
+                is>>song.artista;
+                if(song.artista=="X"){
+                    return is;
+                }
+            cout<<"Duración: ";
+                is>>song.duracion;
+                if(song.duracion==0){
+                    return is;
+                }
+            return is;
+        }
+
+        bool operator==(const Song& other) const {
+            return nombre == other.nombre && artista == other.artista && duracion == other.duracion;
+        }   
 };
+
+class Node {
+    private:
+    Song song;
+    Node* next;
+
+    public:
+    Node(Song song, Node* next) : song(song), next(next) {}
+
+    void setNext(Node* node) {
+        next = node;
+    }
+
+    Node* getNext(){
+        return next;
+    }
+    Song getSong(){
+        return song;
+    }
+};
+
+
 
 class SpotifyJam {
     Node* jam;
 public:
-    list<string> listaGeneral;
-    list<string> lista1;
-    list<string> lista2;
+    list<Song> listaGeneral;
+    list<Song> lista1;
+    list<Song> lista2;
 
 public:
 
     SpotifyJam() : jam(nullptr) {}
 
-    void addListaGeneral(string song) {
+    void addListaGeneral(Song song) {  
         listaGeneral.push_back(song);
     }
 
-    void addLista1(string song) {
+    void addLista1(Song song) {
         lista1.push_back(song);
     }
 
-    void addLista2(string song) {
+    void addLista2(Song song) {
         lista2.push_back(song);
     }
 
-    void removeLista1(string song) {
+    void removeLista1(Song song) {
         lista1.remove(song);
     }
 
-    void removeLista2(string song) {
+    void removeLista2(Song song) {
         lista2.remove(song);
     }
 
-    void removeListaGeneral(string song) {
+    void removeListaGeneral(Song song) {
         listaGeneral.remove(song);
     }
 
     void displayListaGeneral() {
-        for (string song : listaGeneral) {
-            cout << song << endl;
+        for (Song song : listaGeneral) {
+            cout<<"Nombre: "<<song.nombre<<", Artista: "<<song.artista<<", Duración: "<<song.duracion<<" minutos"<<endl;
         }
     }
 
     void displayLista1() {
-        for (string song : lista1) {
-            cout << song << endl;
+        for (Song song : lista1) {
+            cout<<"Nombre: "<<song.nombre<<", Artista: "<<song.artista<<", Duración: "<<song.duracion<<" minutos"<<endl;
         }
     }
 
     void displayLista2() {
-        for (string song : lista2) {
-            cout << song << endl;
+        for (Song song : lista2) {
+            cout<<"Nombre: "<<song.nombre<<", Artista: "<<song.artista<<", Duración: "<<song.duracion<<" minutos"<<endl;
         }
     }
 
     void createJam() {
         Node* tail = nullptr;
-        for (string song : lista1) {
+        for (Song song : lista1) {
             Node* newNode = new Node{song, nullptr};
             if (jam == nullptr) {
                 jam = newNode;
             } else {
-                tail->next = newNode;
+                tail->setNext(newNode);
             }
             tail = newNode;
         }
-        for (string song : lista2) {
+        for (Song song : lista2) {
             Node* newNode = new Node{song, nullptr};
             if (jam == nullptr) {
                 jam = newNode;
             } else {
-                tail->next = newNode;
+                tail->setNext(newNode);
             }
             tail = newNode;
         }
@@ -88,33 +138,34 @@ public:
     void displayJam() {
         Node* temp = jam;
         while (temp != nullptr) {
-            cout << temp->data << endl;
-            temp = temp->next;
+            Song song = temp->getSong();
+            cout<<"Nombre: "<<song.nombre<<", Artista: "<<song.artista<<", Duración: "<<song.duracion<<" minutos"<<endl;
+            temp = temp->getNext();
         }
     }
 };
 
 int main() {
     SpotifyJam spotifyJam;
-    string song1, song2, song3;
+    Song song1, song2, song3;
     int eleccion, eleccion1;
     bool continuar = true;
 
-    while(continuar==true){
+    while(continuar){
         cout<<"Bienvenido a Spotify Jam, ¿Qué deseas hacer?.\n(1) Para agregar canciones a la Lista General\n(2) Para agregar canciones a la Lista 1.\n(3) Para agregar canciones a la Lista 2.\n(4) Mostrar el contenido de las listas.\n(5) Mostrar el Jam de Spotify.\n(6) Eliminar contenido de las listas."<<endl;
         cin>>eleccion;
 
         switch(eleccion){
             case 1:
-                cout<<"Agrega canciones a la Lista General (ingresa 'X' para dejar de agregar): "<<endl;
-                while(cin >> song1 && song1 != "X") {
+                cout<<"Agrega canciones a la Lista General (ingresa 'X' o '0' (En duración) para dejar de agregar): "<<endl;
+                while(cin >> song1 && song1.nombre != "X" && song1.artista != "X" && song1.duracion != 0) {
                     spotifyJam.addListaGeneral(song1);
                 }
                 break;
             case 2:
             spotifyJam.displayListaGeneral();
-            cout<<"Agrega canciones a la lista 1 de la Lista General (ingresa 'X' para dejar de agregar): "<<endl;
-            while (cin >> song2 && song2 != "X") {
+            cout<<"Agrega canciones a la lista 1 de la Lista General (ingresa 'X' o '0' (En duración) para dejar de agregar): "<<endl;
+            while (cin >> song2 && song2.nombre != "X" && song2.artista != "X" && song2.duracion != 0) {
                 if (find(spotifyJam.listaGeneral.begin(), spotifyJam.listaGeneral.end(), song2) != spotifyJam.listaGeneral.end()) {
                     spotifyJam.addLista1(song2);
                 } else{
@@ -126,8 +177,8 @@ int main() {
             break;
             case 3:
                 spotifyJam.displayListaGeneral();
-                cout<<"Agrega canciones a la lista 2 de la Lista General (ingresa 'X' para dejar de agregar): "<<endl;
-                while (cin>>song3 && song3 != "X") {
+                cout<<"Agrega canciones a la lista 2 de la Lista General (ingresa 'X' o '0' (En duración) para dejar de agregar): "<<endl;
+                while (cin>>song3 && song3.nombre != "X" && song3.artista != "X" && song3.duracion != 0) {
                     if (find(spotifyJam.listaGeneral.begin(), spotifyJam.listaGeneral.end(), song3) != spotifyJam.listaGeneral.end()) {
                         spotifyJam.addLista2(song3);
                     }else{
